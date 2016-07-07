2fd29f58bd8bbd59485b092e210c666fd1864489
Alan Savage <alansavage.dev@gmail.com>
2016-07-07 07:32:05 +0100

    Initial commit of project files

---
+++ RandomBytesGenerator/src/RandomBytesGenerator/RandomBytesGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Savage
+{
+    public static class RandomBytesGenerator
+    {
+        public static byte[] Generate(int arraySize)
+        {
+            if (arraySize < 1)
+                throw new ArgumentException("Array size cannot be less than 1");
+
+            System.Security.Cryptography.RandomNumberGenerator rng = System.Security.Cryptography.RandomNumberGenerator.Create();
+            var array = new byte[arraySize];
+            rng.GetBytes(array);
+
+            return array;
+        }
+
+        public static string GenerateAsBase64(int arraySize)
+        {
+            return Convert.ToBase64String(Generate(arraySize));
+        }
+    }
+}
---
+++ RandomBytesGenerator/test/UnitTests/RandomNumberGeneratorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Savage
+{
+    public class RandomBytesGeneratorTests
+    {
+        [Fact]
+        public void ThrowsExceptionWhenGenerateGivenZeroValue()
+        {
+            Assert.Throws<ArgumentException>(() => RandomBytesGenerator.Generate(0));
+        }
+
+        [Theory]
+        [InlineData(8)]
+        [InlineData(20)]
+        [InlineData(512)]
+        public void GenerateCreatesArrayOfCorrectSize(int size)
+        {
+            var sut = RandomBytesGenerator.Generate(size);
+            Assert.Equal(size, sut.Length);
+        }
+
+        [Fact]
+        public void GenerateCreatesDifferentSequence()
+        {
+            var a = RandomBytesGenerator.Generate(10);
+            var b = RandomBytesGenerator.Generate(10);
+
+            Assert.False(a.SequenceEqual(b));
+        }
+
+        [Fact]
+        public void GenerateAsBase64CreatesDifferentSequence()
+        {
+            var a = RandomBytesGenerator.GenerateAsBase64(10);
+            var b = RandomBytesGenerator.GenerateAsBase64(10);
+
+            Assert.NotEqual(a, b);
+        }
+    }
+}
